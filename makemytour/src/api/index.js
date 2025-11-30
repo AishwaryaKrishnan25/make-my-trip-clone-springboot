4139e3142b450c77eeda26aea1ae61a8edb8346f
Aishwarya Krishnan <aishwarya.k2503@gmail.com>
2025-11-30 15:09:43 +0530

    Update index.js

--- makemytour/src/api/index.js
+++ makemytour/src/api/index.js
@@ -1,7 +1,7 @@
 // src/api/index.js
 import axios from "axios";
 
-const BACKEND_URL = process.env.NEXT_PUBLIC_API_BASE || "https://make-my-trip-clone-springboot-fy0w.onrender.com";
+const BACKEND_URL = process.env.NEXT_PUBLIC_API_BASE || "http://localhost:8080";
 
 /* ----------------- Auth / User ----------------- */
 export const login = async (email, password) => {
